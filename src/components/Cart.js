e2044696821f85d04e73277f42d6e2e07236768f
5yndr0m <sdilanjana21@gmail.com>
2024-12-03 18:13:21 +0530

    feat: creating cart logic

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,7 @@
-export default function Cart() {
+import { useState } from "react";
+
+export default function Cart({ cartItems }) {
+    const [gTotal, setGTotal] = useState(0);
     
     return (
         <div className="table-container">
@@ -12,11 +15,24 @@ export default function Cart() {
 
                 </thead>
                 {
-                    
+                    cartItems.map((cartItem) => (
+                      <tr>
+                        <td>
+                          {cartItem.name}
+                        </td>
+                        <td>
+                          {cartItem.qunt}
+                        </td>
+                        <td>
+                          {cartItem.price}
+                        </td>
+                        {() => { setGTotal(gTotal + cartItem.price * cartItem.qunt) }}
+                      </tr>
+                    ))
                 }
                 <tr>
                     <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
+            <td colSpan={2}>{ gTotal }</td>
                 </tr>
             </table>
            
